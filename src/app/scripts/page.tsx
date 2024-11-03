f49e38498c9b0d5562d6794ec8fb5b5f6cf56745
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-11-03 21:24:34 +0100

    Wrap loading state with Suspense for better handling of asynchronous script loading in the Page component

--- src/app/scripts/page.tsx
+++ src/app/scripts/page.tsx
@@ -6,7 +6,7 @@ import ScriptItem from "@/app/scripts/_components/ScriptItem";
 import { Category } from "@/lib/types";
 import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Sidebar from "./_components/Sidebar";
 
 export default function Page() {
@@ -65,34 +65,32 @@ export default function Page() {
     });
   };
 
-  if (loading) {
-    return (
+return (
+    <Suspense fallback={
       <div className="flex h-screen w-full flex-col items-center justify-center gap-5 bg-background px-4 md:px-6">
         <div className="space-y-2 text-center">
           <Loader2 className="h-10 w-10 animate-spin" />
         </div>
       </div>
-    );
-  }
-
-  return (
-    <div className="mb-3">
-      <div className="mt-20 flex sm:px-4 xl:px-0">
-        <div className="hidden sm:flex">
-          <Sidebar
-            items={links}
-            selectedScript={selectedScript}
-            setSelectedScript={setSelectedScript}
-          />
-        </div>
-        <div className="mx-7 w-full sm:mx-0 sm:ml-7">
-          <ScriptItem
-            items={links}
-            selectedScript={selectedScript}
-            setSelectedScript={setSelectedScript}
-          />
+    }>
+      <div className="mb-3">
+        <div className="mt-20 flex sm:px-4 xl:px-0">
+          <div className="hidden sm:flex">
+            <Sidebar
+              items={links}
+              selectedScript={selectedScript}
+              setSelectedScript={setSelectedScript}
+            />
+          </div>
+          <div className="mx-7 w-full sm:mx-0 sm:ml-7">
+            <ScriptItem
+              items={links}
+              selectedScript={selectedScript}
+              setSelectedScript={setSelectedScript}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }

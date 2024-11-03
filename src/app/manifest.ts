ffe4e663e8c832b1e9d34d1888e0eb8d62b68556
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-11-03 21:34:57 +0100

    Implement static params generation in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 
-export const dynamic = "force-static";
+export const generateStaticParams = () => {
+  return [];
+};
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
